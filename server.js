b51b189021e9f5108ae29a2b6268ebda13287b72
screaminime <screaminime@gmail.com>
2015-05-15 21:07:24 -0500

    It's  a NEW world!!

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var Datastore = require('nedb');
 var db = new Datastore({ filename: 'db/DotaMatches.db', autoload: true });
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Hello New World');
 });
 
 var server = app.listen(process.env.PORT || 3000);
